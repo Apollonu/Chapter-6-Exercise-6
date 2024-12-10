84b50b3fe08cba463dbd9a12213175fe90b4a828
Apollo <kingfrostalpha@gmail.com>
2024-12-10 09:32:27 -0800

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -3,9 +3,9 @@
     Programmer: Harrison Hudgins
     Date: December 10, 2024
     Requirements:
-    For this programming problem, download the following files from the book’s companion website:
-    • Teams.txt—This file contains a list of several Major League baseball teams in alphabetical order. Each team listed in the file has won the World Series at least once.
-    • WorldSeriesWinners.txt—This file contains a chronological list of the World Series’ winning teams from 1903 to 2012. (The first line in the file is the name of the team that won in 1903, and the last line is the name of the team that won in 2012. Note the World Series was not played in 1904 or 1994.)
+    For this programming problem, download the following files from the bookâ€™s companion website:
+    â€¢ Teams.txtâ€”This file contains a list of several Major League baseball teams in alphabetical order. Each team listed in the file has won the World Series at least once.
+    â€¢ WorldSeriesWinners.txtâ€”This file contains a chronological list of the World Seriesâ€™ winning teams from 1903 to 2012. (The first line in the file is the name of the team that won in 1903, and the last line is the name of the team that won in 2012. Note the World Series was not played in 1904 or 1994.)
     Write a program that displays the contents of the Teams.txt file on the screen and prompts the user to enter the name of one of the teams. The program should then display the number of times that team has won the World Series in the time period from 1903 to 2012.
 */
 
@@ -66,6 +66,7 @@ void winnersArray() {
         getline(winnerFile, line);
         winnerArray[team] = line;
     }
+    winnerFile.close();
 }
 
 int timesWon(string choice) {
